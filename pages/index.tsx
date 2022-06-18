1f934f4d5e4faec83fea524d1f960835586efd9b
Kelvin Brito <britok30@gmail.com>
2022-06-18 01:18:31 -0400

    minor fixes

--- pages/index.tsx
+++ pages/index.tsx
@@ -58,15 +58,16 @@ const Home = ({
                   objectFit="cover"
                   alt="main-game-image"
                 />
-                <h1 className="text-2xl md:text-8xl absolute bottom-[10rem] left-10 md:left-20 font-bold">
+                <h1 className="text-4xl md:text-8xl absolute bottom-[10rem] left-10 md:left-20 font-bold">
                   {mainGame.name}
                 </h1>
-                <div className="w-fit flex absolute bottom-[5rem] left-60 border p-2 rounded-lg">
-                  {mainGame.ratings_count} count
-                </div>
 
                 <div className="text-xl border p-2 rounded-lg absolute bottom-[5rem] left-10 md:left-20 font-bold">
                   {mainGame.metacritic || 0} Metacritic
+                </div>
+
+                <div className="w-fit flex absolute bottom-[5rem] left-48 md:left-60 border p-2 rounded-lg">
+                  {mainGame.ratings_count} count
                 </div>
               </a>
             </div>
